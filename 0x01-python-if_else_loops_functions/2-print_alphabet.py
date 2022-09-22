2c0e22b366034efadcaa145c10e848fa8d08df8c
LoloDum <chinedumokamgba@gmail.com>
2022-09-22 07:01:10 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(["{}".format(chr(a)) for a in range(97, 123) if chr(a) not in "eq"]), end="")
+for character in range(97, 123):
+    print("{:c}".format(character), end='')
